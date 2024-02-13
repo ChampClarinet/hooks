bf55e0a670e269eec7a055d4d410f3323187a911
Wallop Opasakhun (macbook) <champbassist159@gmail.com>
2024-02-14 02:41:01 +0700

    modify useDebounce to make it triggered manually

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,27 @@ import { useCallback, useEffect } from "react";
 
 /**
  * Debounces a function call by delaying its execution.
- * @param handler The function to be debounced
- * @param delay The delay in milliseconds before executing the function (default is 1000ms)
+ * @param {Function} handler The function to be debounced.
+ * @param {number} [delay=1000] The delay in milliseconds before executing the function (default is 1000ms).
+ * @returns {Function} The debounced version of the handler function.
  */
-export const useDebounce = (handler: () => void, delay = 1000) => {
+export const useDebounce = (
+  handler: () => void,
+  delay: number = 1000
+): Function => {
+  /**
+   * Debounced version of the handler function.
+   * @returns {void}
+   */
   const debouncedHandler = useCallback(() => {
     const timeout = setTimeout(handler, delay);
     return () => clearTimeout(timeout);
   }, [handler, delay]);
 
+  // Call the debounced handler effect
   useEffect(() => {
     return debouncedHandler();
   }, [debouncedHandler]);
+
+  return debouncedHandler;
 };
